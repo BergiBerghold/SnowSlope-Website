e268af4f85502b5807afc0946bbb515178a5f387
minimilch <natrium-linux>
2022-03-21 14:46:26 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'leaflet/dist/leaflet.css';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Map from './components/Map/Map';
+import Params from './components/Params/Params';
+import Loading from "./components/Loading/Loading";
+import DisplayMap from "./components/DisplayMap/DisplayMap";
+import Start from './components/Start/Start'
+
+
+function App() {
+    return (
+        <main>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/start" element={<Start/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/map" element={<Map/>} />
+                <Route path="/params" element={<Params/>} />
+                <Route path="/loading" element={<Loading/>} />
+                <Route path="/results" element={<DisplayMap/>} />
+            </Routes>
+        </main>
+    )
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
